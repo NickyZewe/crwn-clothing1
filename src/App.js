6a9e7e479102d3a2115f4ec403b84b52b62257ed
NickyZewe <thenzewe@yahoo.com>
2023-02-28 14:36:04 -0500

    added basic layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
 const App = () => {
-  return <div>hello world</div>;
+  return;
+  <div className="categories-container">
+    <div className="category-container">
+      // <img />
+      <div className="category-body-container">
+        <h2>Hats</h2>
+        <p>Show Now</p>
+      </div>
+    </div>
+    <div className="category-container">
+      // <img />
+      <div className="category-body-container">
+        <h2>Jackets</h2>
+        <p>Show Now</p>
+      </div>
+    </div>
+    <div className="category-container">
+      // <img />
+      <div className="category-body-container">
+        <h2>Sneakers</h2>
+        <p>Show Now</p>
+      </div>
+    </div>
+    <div className="category-container">
+      // <img />
+      <div className="category-body-container">
+        <h2>Womens</h2>
+        <p>Show Now</p>
+      </div>
+    </div>
+    <div className="category-container">
+      // <img />
+      <div className="category-body-container">
+        <h2>Mens</h2>
+        <p>Show Now</p>
+      </div>
+    </div>
+  </div>;
 };
 
 export default App;
