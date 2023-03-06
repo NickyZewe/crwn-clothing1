8bccb63dc4e249bc27b6e73daef69613f5077c25
NickyZewe <thenzewe@yahoo.com>
2023-03-05 20:13:41 -0500

    cart items component finished

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -14,9 +14,13 @@ const CartDropdown = () => {
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
       </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -27,10 +27,11 @@ export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
 
-  const addItemToCart = (productToAdd) => {
-    setCartItems(addCartItem(cartItems, productToAdd));
+  const addItemToCart = (product) => {
+    setCartItems(addCartItem(cartItems, product));
   };
 
   const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
+
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
