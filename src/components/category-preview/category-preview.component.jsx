a3865a262e65b6c8e974a2c000373fd8c105d506
NickyZewe <thenzewe@yahoo.com>
2023-03-07 12:15:02 -0500

    added links to category preview titles

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,12 +1,13 @@
 import "./category-preview.styles.scss";
 import ProductCard from "../product-card/product-card.component";
+import { Link } from "react-router-dom";
 
 const CategoryPreview = ({ title, products }) => {
   return (
     <div className="category-preview-container">
-      <h2>
-        <span className="title">{title.toUpperCase()}</span>
-      </h2>
+      <Link className="title" to={title}>
+        <h2>{title.toUpperCase()}</h2>
+      </Link>
       <div className="preview">
         {products
           .filter((_, idx) => idx < 4)
